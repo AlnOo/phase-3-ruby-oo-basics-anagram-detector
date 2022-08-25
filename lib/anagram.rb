bbce3bfdc50abc21dde2c3125866a5e10b341da7
Allan <github email address>
2022-08-25 17:20:08 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word
+    end
+  
+    def match array
+      matches = []
+      array.each do |word|
+        if word.chars.sort.join == @word.chars.sort.join
+          matches << word
+        end
+      end
+      return matches
+    end
+    
+  end
